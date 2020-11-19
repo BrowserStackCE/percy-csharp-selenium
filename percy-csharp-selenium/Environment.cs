277ab53831911ca381439f3404764055a31daa80
Madhav Wagle <architwagle@gmail.com>
2020-11-19 21:27:09 +0530

    added empty capability handling to getenvironment method

--- percy-csharp-selenium/Environment.cs
+++ percy-csharp-selenium/Environment.cs
@@ -27,9 +27,9 @@ namespace percy_csharp_selenium
         public String getEnvironmentInfo()
         {
             ICapabilities cap = ((IHasCapabilities)this.driver).Capabilities;
-            String os = cap.GetCapability("platformName").ToString().ToLower();
-            String browserName = cap.GetCapability("browserName").ToString().ToLower();
-            String version = cap.GetCapability("browserVersion").ToString().ToLower();
+            String os = cap.HasCapability("platformName") ? cap.GetCapability("platformName").ToString().ToLower() : "no_platform_name";
+            String browserName = cap.HasCapability("browserName") ? cap.GetCapability("browserName").ToString().ToLower() : "no_browser_name";
+            String version = cap.HasCapability("browserVersion") ? cap.GetCapability("browserVersion").ToString().ToLower() : "no_browser_ver";
             return String.Format("selenium-csharp; %s; %s/%s", os, browserName, version);
         }
     }
